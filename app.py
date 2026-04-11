104c6cbac5c19d30bdada283dd8c9e3031a8cc29
akash1234-design <singh783072@gmail.com>
2026-04-11 23:32:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ else:
     genai.configure(api_key=api_key)
 
    # Load model
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 import pandas as pd
 
